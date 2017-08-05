b708ea2f82afb964d331fa74b25fcd31a4c93bdb
juankboards <juankboards@gmail.com>
2017-08-05 03:25:27 -0400

    add select website to request the search

--- app/controllers/web_scraping_controller.rb
+++ app/controllers/web_scraping_controller.rb
@@ -1,4 +1,5 @@
 class WebScrapingController < ApplicationController
+
 	require 'capybara'
 	require 'capybara/poltergeist'
 
@@ -12,25 +13,51 @@ class WebScrapingController < ApplicationController
 	
 
 	def search()
-		url = "http://www.sephora.com/search/search.jsp?keyword=#{params[:search]}&mode=all&pageSize=20&currentPage=1"
-		# @results = get_search_results(url)
-			
-		@results = find_element(url)
+		search_without_spaces = params[:search].strip.gsub(/\s+/, "%20")
+		capybara_params = get_website_scraping_structure(params[:website], search_without_spaces)		
+		@results = scrap_website(capybara_params)
 	end
 
-	# def get_search_results(url)
-	# 	array = find_element(url)
-	# end
+	def get_website_scraping_structure(website, search)
+		case website
+		when "sephora"
+			url = "http://www.sephora.com/search/search.jsp?keyword=#{search}&mode=all&pageSize=20&currentPage=1"
+			css_selector = ["img.FlexEmbed-content", "a.SkuItem", "div.u-fwb span", "div.SkuItem-nameBrand", "div.SkuItem-nameBrand"]
+			capybara_methods = ["[]", "[]", "text", "text", "text"]
+			capybara_parameters = ["seph-lazy-src", "href", "", "", ""]
+			hash_keys = [:img, :link, :price, :name, :brand]
+		when "asos"
+			url = "http://us.asos.com/search/#{search}?q=#{search}&pgesize=20"
+			css_selector = ["img.product-img", "a.product-link", "span.price", "span.name"]
+			capybara_methods = ["[]", "[]", "text", "text"]
+			capybara_parameters = ["src", "href", "", ""]
+			hash_keys = [:img, :link, :price, :name]
+		when "ulta"
+			url = "http://www.ulta.com/ulta/a/_/Ntt-#{search}?Nrpp=20&ciSelector=searchResults"
+			css_selector = ["a.product img", "a.product", "span.regPrice", "p.prod-desc a", "h4.prod-title a"]
+			capybara_methods = ["[]", "[]", "text", "text", "text"]
+			capybara_parameters = ["src", "href", "", "", ""]
+			hash_keys = [:img, :link, :price, :name, :brand]
+		when "frends"
+			url = "http://www.frendsbeauty.com/catalogsearch/result/index/?limit=24&q=#{search}"
+			css_selector = ["a.product-image img", "a.product-image", "span.price", "h2.product-name"]
+			capybara_methods = ["[]", "[]", "text", "text"]
+			capybara_parameters = ["src", "href", "", ""]
+			hash_keys = [:img, :link, :price, :name]
+		end
+		{url: url, css_selector: css_selector, method: capybara_methods, parameters: capybara_parameters, keys: hash_keys}
+	end
 
-	def find_element(url)		
+	def scrap_website(capybara_params)		
+		scraped_info = Hash.new
 		session = Capybara.current_session
-		session.visit(url)	
+		session.visit(capybara_params[:url])	
 		sleep(1)
-		brand_array = session.all("div.SkuItem-nameBrand", :wait => 5).map {|element| element.text}
-		name_array = session.all("div.SkuItem-nameDisplay").map {|element| element.text}
-		price_array = session.all("div.u-fwb span").map {|element| element.text}
-		img_array = session.all("img.FlexEmbed-content", :visible => false, :wait => 5).map {|element| element['seph-lazy-src']}
-		link_array = session.all("a.SkuItem").map {|element| element['href']}
-		scraped_info = {brand: brand_array, name: name_array, price: price_array, img: img_array, link: link_array}
+
+		capybara_params[:keys].each_with_index do |key, index|
+			scraped_info[key] = session.all(capybara_params[:css_selector][index], :visible => false, :wait => 5).map {|element| element.method(capybara_params[:method][index]).call(capybara_params[:parameters][index])}
+		end
+
+		scraped_info
 	end
 end
