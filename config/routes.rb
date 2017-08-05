1e31e64cfecad153ea7a2ac63c0da7ea9848cfa8
juankboards <juankboards@gmail.com>
2017-08-04 22:55:28 -0400

    scraping 20 results from sephora search

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'web_scraping#index'
+  get 'search', to: 'web_scraping#search'
 end
